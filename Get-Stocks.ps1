be1448b0084e8a461527997013ed5f2ca42586bb
tnilkuha <tnilkuha@gmail.com>
2016-03-28 15:38:43 +0700

    Initial version
    Your task is to create a small powershell app that will have the
    following possible arguments either a single argument like so
    Get-Stocks AAPL or Get-Stocks -File stocklist.txt where stocklist.txt
    contains a newline delimited list of stock tickers like:
    AAPL
    IBM
    SAP
    The program will then output the stock name and the most recent stock
    price eg
    AAPL 105.91
    or if a list:
    AAPL 105.91
    IBM 148.66
    SAP 79.62

---
+++ Get-Stocks.ps1
@@ -0,0 +1,43 @@
+﻿## Powershell script called "Get-Stocks.ps1" which takes the US stock quote or a text file contains newline delimited list of stock tickers like
+## -File stocklist.txt where stocklist.txt contains a newline delimited list of stock tickers like:
+## AAPL
+## IBM
+## SAP
+## The script will then return the stock name and the most recent stock price from Yahoo! Finance.
+## Author: Triphol "Pao" Nilkuha
+## Email: tnilkuha@gmail.com
+## Date: 28 Mar 2016
+## Development Env: PowerShell 5.0 on MS Windows 10.
+
+
+
+# take stock symbol from command line and pass an argument -File for a text file contains a newline delimited list of stock symbols.
+param(
+     [string] $symbol,
+     [string]$File
+
+)
+
+# Check to see if there is any argument or parameter when calling the script.
+If(($symbol) -or ($File)){
+
+$prefixurl = "http://download.finance.yahoo.com/d/quotes.csv?s=" # yahoo finance url prefix
+$subfixurl = "&f=l1" # subfix url
+
+If($File){
+        If(($File) -and ((Get-Content $File) -eq $Null)){"Your stock tickers text file '$File' is blank."}
+        else{
+
+        foreach ($symbol in Get-Content $File | sort-object) {
+        $symbol.ToUpper() + " " + (Invoke-RestMethod -Uri "$prefixurl + $symbol + $subfixurl" -Method Get)
+            }
+                }
+    }
+Else {
+    $symbol.ToUpper() + " " + (Invoke-RestMethod -Uri "$prefixurl + $symbol + $subfixurl" -Method Get)}
+
+}
+
+# If there is no argument or parameter (a stock quote symbol or -File) enter, prompt the screen with this message
+
+Else {Write-Host "Please specify a US stock quote symbol or -File Textfilename.txt for list of stock tickers `n(e.g.'Get-Stocks.ps1 ibm' or 'Get-Stocks.ps1 -File stocklist.txt')"}
